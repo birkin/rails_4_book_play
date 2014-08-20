4287c2f9d611f4cf812d5ae8d96f759834d8dcdb
birkin <birkin.diana@gmail.com>
2014-08-20 17:31:11 -0400

    adds time to sidebar (css under construction).

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,7 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order( :title )
+    cur_time = Time.now
+    @cur_time_string = "#{cur_time.strftime( '%Y-%m-%d, %I:%M%P' )}"
   end
 end
